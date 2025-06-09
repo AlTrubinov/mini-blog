e2bbe473fd827cf84d7ba4dee8e657d8a3e276a5
Aleksei <altrubinov26@gmail.com>
2025-06-09 16:07:15 +0700

    init

---
+++ cmd/mini-blog/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"mini-blog/internal/config"
+)
+
+func main() {
+	cfg := config.NewConfig()
+
+	_ = cfg
+
+	// TODO: init logger
+
+	// TODO: init storage
+
+	// TODO: init handler
+
+	// TODO: run server
+}
---
+++ internal/config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env string `yaml:"env" env-required:"true"`
+	Database
+	HTTPServer
+}
+
+type Database struct {
+	Host     string `yaml:"host" env-required:"true"`
+	Port     int    `yaml:"port" env-required:"true"`
+	User     string `yaml:"user" env-required:"true"`
+	Password string `yaml:"password" env-required:"true"`
+	Name     string `yaml:"name" env-required:"true"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-required:"true"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func NewConfig() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH environment variable not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var config Config
+
+	if err := cleanenv.ReadConfig(configPath, &config); err != nil {
+		log.Fatal("loading config error: ", err)
+	}
+
+	return &config
+}
